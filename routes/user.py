7158d994583f75b282b0cdba3ce22b549f20143c
SebySurfer <sebysurfer@gmail.com>
2024-09-24 21:27:58 -0600

    Commit_3
    Full CRUD

--- routes/user.py
+++ routes/user.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from models.user import User 
 from config.db import conn
 from schemas.user import userEntity, usersEntity
+from bson import ObjectId
 user = APIRouter()
 
 @user.get('/')
@@ -11,8 +12,25 @@ async def find_all_users():
     print(usersEntity(conn.local.user.find()))
     return usersEntity(conn.local.user.find())
 
+@user.get('/{id}')
+async def find_one_user(id):
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+
 @user.post('/')
 async def create_user(user: User):
     conn.local.user.insert_one(dict(user))
     return usersEntity(conn.local.user.find())
+
+@user.put('/{id}')
+async def update_user(id, user: User):
+    conn.local.user.find_one_and_update({"_id": ObjectId(id)}, {
+        "$set": dict(user)
+    })
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+
+
+@user.delete('/{id}')
+async def delete_user(id, user: User):
+    return userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)}))
+
 
